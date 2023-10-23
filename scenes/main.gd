7b11a17a15cde01ef01c70dda0c00f7105ef414a
Ethan Crann <ecrann@protonmail.com>
2023-10-23 00:04:03 -0230

    Rotating y at random degrees

--- scenes/main.gd
+++ scenes/main.gd
@@ -13,10 +13,11 @@ func _process(delta):
 
 func _on_meat_sphere_spawn_blood(pos, nor):
 	var blood_stain := blood_scene.instantiate();
+	var random_y_rotation = randf_range(0,360);
 	blood_stains.add_child(blood_stain)
 	blood_stain.global_transform.origin = pos;
+	blood_stain.rotate_y(deg_to_rad(random_y_rotation));
 	blood_stain.basis.y = nor;
 	blood_stain.basis.x = -blood_stain.basis.z.cross(-nor)
-	#blood_stain.global_transform.basis = nor;
-	#blood_stain.look_at(pos+Vector3.UP,Vector3.UP)
+	blood_stain.basis = blood_stain.basis.orthonormalized()
 	pass
