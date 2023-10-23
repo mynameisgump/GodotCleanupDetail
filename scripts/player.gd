0919517cc106cf4ced393279029b9adf2483e217
Ethan Crann <ecrann@protonmail.com>
2023-10-23 18:52:57 -0230

    Weeee bouncy :)

--- scripts/player.gd
+++ scripts/player.gd
@@ -92,7 +92,7 @@ func on_grabber_collision(collision_object):
 		if Input.is_action_just_pressed("left_mouse"): 
 			grabbed_item_rel_pos = head.to_local(collision_object.position) 
 			grabbed_item = collision_object 
-			grabbed_item.apply_central_impulse(Vector3.UP * 0.5)
+			grabbed_item.apply_central_impulse(Vector3.UP * 1)
 
 func can_be_picked(object): 
 	return object.has_method("update_velocity") 
